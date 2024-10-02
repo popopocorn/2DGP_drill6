6774999f8dbd89f1784eb3f60536925d00008b33
popopocorn <popcorn375@naver.com>
2024-10-02 14:44:11 +0900

    손이 랜덤하게 나타남

--- .venv/Scripts/2021180001_random_hand.py
+++ .venv/Scripts/2021180001_random_hand.py
@@ -1,4 +1,43 @@
 from pico2d import *
+from random import *
+
+width=800
+height=600
+
+open_canvas(width, height)
+hand = load_image('hand_arrow.png')
+character = load_image('animation_sheet.png')
+ground=load_image('TUK_GROUND_FULL.png')
+
+hand_flag=True
+hand_x=0
+hand_y=0
+char_x=0
+char_y=0
+dx=0
+dy=0
+
+def random_hand():
+    global hand_x,hand_y,hand_flag
+    if hand_flag:
+        hand_x=randint(0, 800)
+        hand_y=randint(0, 600)
+        hand_flag=False
+def trace_hand():
+    global hand_x,hand_y,hand_flag,char_x,char_y
+    if not hand_flag:
+        pass
+def check():
+    pass
+def draw_scene():
+    clear_canvas()
+    hand.draw(hand_x, hand_y)
+    update_canvas()
 
 
+while True:
 
+    random_hand()
+    trace_hand()
+    check()
+    draw_scene()
